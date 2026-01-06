1678f0d28b02143f255da4ad36d3a7c396f1ccab
KarthikeyanTigerAnalytics <karthikeyan.balakrishnan@tigeranalytics.com>
2026-01-06 15:05:37 -0500

    folder structure

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir src
+mkdir test
+
+touch src/__init__.py
+touch test/notebook_1.ipnb
+touch .env
+touch app.py
+touch requirements.txt
+touch setup.py
+touch src/prompt.py
+touch src/utils.py
+
+echo "Files and directories created successfully."
